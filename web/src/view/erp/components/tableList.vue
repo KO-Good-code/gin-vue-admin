999c03d73a292e1919c90e7962b0f2dd7c8e3d3f
chenrui <734064248@qq.com>
2021-08-15 13:43:17 +0800

    竞价demo v1.0

--- web/src/view/erp/components/tableList.vue
+++ web/src/view/erp/components/tableList.vue
@@ -5,6 +5,7 @@
       :label="data.label"
       :show-overflow-tooltip="true"
       align="center"
+      sortable
       >
       <template slot-scope="scope">
         <el-select v-if="data.type === 'select'" v-model="scope.row[data.key]" placeholder="请选择">
@@ -15,7 +16,8 @@
             :value="item.value">
           </el-option>
         </el-select>
-        <el-input v-else v-model="scope.row[data.key]" :placeholder="data.placeholder"></el-input>
+        <el-input v-else-if="data.type === 'input'" v-model="scope.row[data.key]" :placeholder="data.placeholder"></el-input>
+        <span v-else>{{scope.row[data.key]}}</span>
       </template>
     </el-table-column>
 </template>
@@ -62,33 +64,8 @@ export default {
   mounted() {},
 
   methods: {
-    applyStatusFilters(val) {
-      const result = {
-        0: "待审核",
-        1: "审核通过",
-        2: "审核失败",
-      }
-      return result[val] || result[0];
-    },
-    expiredFilters(val) {
-      const result = {
-        0: "无效",
-        1: "有效",
-      }
-      return result[val] || "";
-    },
-    authSourceFilters(val) {
-      const result = {
-        1: "我的",
-        2: "圈子",
-      }
-      return result[val] || "";
-    },
-    workCityFilters(val, row) {
-      return `${val} - ${row.workCity}${row.workTown}`
-    },
-    Allfilters(name, val, row) {
-      return this[name] && this[name](val, row)
+    filterHandler(value, row, column){
+
     }
   }
 }
--- web/src/view/erp/index.vue
+++ web/src/view/erp/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <el-row :gutter="15">
-      <el-form ref="elForm" :model="formData" :rules="rules" size="medium" label-width="100px">
-        <el-col :span="12">
+      <el-form ref="elForm" :model="formData" inline :rules="rules" size="medium" label-width="150px">
+        <!-- <el-col :span="12">
           <el-form-item label="单行文本" prop="mobile">
             <el-input v-model="formData.mobile" placeholder="请输入手机号" :maxlength="11" show-word-limit clearable
               prefix-icon='el-icon-mobile' :style="{width: '100%'}"></el-input>
@@ -26,20 +26,61 @@
               </el-form-item>
             </el-col>
           </el-row>
-        </el-col>
+        </el-col> -->
+        
+        <el-form-item size="large" label="最高限价抽取比例">
+            <el-input-number v-model="formData.max" :min="0.01" :max="1" :step="0.01" label="描述文字"></el-input-number>
+        </el-form-item>
+        <el-form-item size="large" label="平均价抽取比例">
+            <el-input-number v-model="formData.min" :min="0.01" :max="1" :step="0.01" label="描述文字"></el-input-number>
+        </el-form-item>
+        <el-form-item size="large" label="最高限价">
+            <el-input v-model="formData.num"></el-input>
+        </el-form-item>
         <el-col :span="24">
           <el-form-item size="large">
-            <el-button type="primary" @click="submitForm">提交</el-button>
-            <el-button @click="resetForm">重置</el-button>
+            <el-button type="primary" @click="submitForm">计算</el-button>
+            <el-button @click="resetForm">新增</el-button>
           </el-form-item>
         </el-col>
       </el-form>
     </el-row>
+    <el-row>
+      双底线：{{query.max}}~{{query.min}}
+    </el-row>
+    <el-table
+      ref="multipleTable"
+      :data="list"
+      :default-sort = "{prop: 'num', order: 'descending'}"
+      >
+        <!-- <tableList v-for="(i, t) in listMap" :key="t" :data="i" /> -->
+        <el-table-column
+          property="name"
+          label="公司名称"
+          :show-overflow-tooltip="true"
+          align="center"
+          > </el-table-column>
+          <el-table-column
+          property="num"
+          label="投标价"
+          :show-overflow-tooltip="true"
+          align="center"
+          :filters="[{text:'双底线', value: 1}]"
+          :filter-method="filterHandler"
+          sortable
+          > </el-table-column>
+        <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+          <template slot-scope="scope">
+            <el-button type="text" size="mini" @click="add">修改</el-button>
+            <el-button type="text" size="mini" @click="remove(scope.$index)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
   </div>
 </template>
 <script>
+import tableList from "./components/tableList.vue"
 export default {
-  components: {},
   props: [],
   data() {
     return {
@@ -47,6 +88,10 @@ export default {
         mobile: undefined,
         field118: null,
         field119: undefined,
+      },
+      query: {
+        max:0,
+        min: 0
       },
       rules: {
         mobile: [{
@@ -76,14 +121,71 @@ export default {
         "label": "选项二",
         "value": 2
       }],
+      list: [
+        {
+          name: "四川君羊建设集团有限公司",
+          num: 31149199
+        },
+        {
+          name: "百年建设集团有限公司",
+          num: 31668965
+        },
+        {
+          name: "中恒太鸿建设有限公司",
+          num: 31674722
+        },
+        {
+          name: "中建城开环境建设有限公司",
+          num: 31962687
+        },
+        {
+          name: "四川富祥建筑工程有限公司",
+          num: 31964488
+        },
+        {
+          name: "中诚智翔建设集团有限公司",
+          num: 32102915
+        },
+        {
+          name: "新疆交通建设集团股份有限公司",
+          num: 42997361
+        },
+      ]
     }
   },
-  computed: {},
-  watch: {},
+  computed: {
+    listMap(){
+      return [
+        {
+          key: "name",
+          label: "公司名称"
+        },
+        {
+          key: "num",
+          label: "投标价"
+        },
+        // {
+        //   key: "max",
+        //   label: "最高限价抽取比例"
+        // },
+        // {
+        //   key: "min",
+        //   label: "平均价抽取比例"
+        // },
+      ]
+    }
+  },
+  components: {
+    tableList
+  },
   created() {},
   mounted() {},
   methods: {
     submitForm() {
+      const len = this.list.length;
+      const total = this.list.reduce((i, t) => i + t.num,0);
+      this.query.min = Math.floor(total/len*this.formData.min);
+      this.query.max =Math.floor(this.formData.num*this.formData.max);
       this.$refs['elForm'].validate(valid => {
         if (!valid) return
         // TODO 提交表单
@@ -92,6 +194,11 @@ export default {
     resetForm() {
       this.$refs['elForm'].resetFields()
     },
+    add(){},
+    filterHandler(value, row, column){
+      const property = column['property'];
+      return row[property] >= this.query.min || row[property]>= this.query.max;
+    }
   }
 }
 
