4313ff53a9198ec7eae6d3c93616dd6a0e4af391
chenrui <734064248@qq.com>
2021-08-22 21:11:40 +0800

    完成修改，新增，删除功能

--- web/src/view/erp/index.vue
+++ web/src/view/erp/index.vue
@@ -1,33 +1,7 @@
 <template>
   <div>
     <el-row :gutter="15">
-      <el-form ref="elForm" :model="formData" inline :rules="rules" size="medium" label-width="150px">
-        <!-- <el-col :span="12">
-          <el-form-item label="单行文本" prop="mobile">
-            <el-input v-model="formData.mobile" placeholder="请输入手机号" :maxlength="11" show-word-limit clearable
-              prefix-icon='el-icon-mobile' :style="{width: '100%'}"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="24">
-          <el-row>
-            <el-col :span="9">
-              <el-form-item label="日期范围" prop="field118">
-                <el-date-picker type="daterange" v-model="formData.field118" format="yyyy-MM-dd"
-                  value-format="yyyy-MM-dd" :style="{width: '100%'}" start-placeholder="开始日期"
-                  end-placeholder="结束日期" range-separator="至" clearable></el-date-picker>
-              </el-form-item>
-            </el-col>
-            <el-col :span="9">
-              <el-form-item label="选择" prop="field119">
-                <el-select v-model="formData.field119" placeholder="请选择选择" clearable :style="{width: '100%'}">
-                  <el-option v-for="(item, index) in field119Options" :key="index" :label="item.label"
-                    :value="item.value" :disabled="item.disabled"></el-option>
-                </el-select>
-              </el-form-item>
-            </el-col>
-          </el-row>
-        </el-col> -->
-        
+      <el-form ref="elForm" :model="formData" inline size="medium" >
         <el-form-item size="large" label="最高限价抽取比例">
             <el-input-number v-model="formData.max" :min="0.01" :max="1" :step="0.01" label="描述文字"></el-input-number>
         </el-form-item>
@@ -37,21 +11,30 @@
         <el-form-item size="large" label="最高限价">
             <el-input v-model="formData.num"></el-input>
         </el-form-item>
-        <el-col :span="24">
-          <el-form-item size="large">
-            <el-button type="primary" @click="submitForm">计算</el-button>
-            <el-button @click="resetForm">新增</el-button>
+        <el-row>
+          <el-form-item size="large" label="双底线">
+            {{query.max}}~{{query.min}}
           </el-form-item>
-        </el-col>
+        </el-row>
       </el-form>
-    </el-row>
-    <el-row>
-      双底线：{{query.max}}~{{query.min}}
+      <el-form ref="form"  :model="form" inline :rules="rules" size="medium" @key.enter.native="submitForm('form')">
+        <el-form-item size="large" label="公司名称" prop="name">
+            <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item size="large" label="投标价" prop="num">
+            <el-input v-model.number="form.num"></el-input>
+        </el-form-item>
+        <el-form-item size="large">
+          <!-- <el-button type="primary" @click="submitForm">计算</el-button> -->
+          <el-button icon="el-icon-edit" type="primary" native-type="submit" @click="submitForm('form')">新增</el-button>
+        </el-form-item>
+      </el-form>
     </el-row>
     <el-table
       ref="multipleTable"
       :data="list"
       :default-sort = "{prop: 'num', order: 'descending'}"
+      :row-class-name="tableRowClassName"
       >
         <!-- <tableList v-for="(i, t) in listMap" :key="t" :data="i" /> -->
         <el-table-column
@@ -76,11 +59,29 @@
           </el-table-column>
         <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
           <template slot-scope="scope">
-            <el-button type="text" size="mini" @click="add">修改</el-button>
-            <el-button type="text" size="mini" @click="remove(scope.$index)">删除</el-button>
+            <el-button type="text" size="mini" @click="edit(scope.row)">修改</el-button>
+            <el-button type="text" size="mini" @click="remove(scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
+      <el-dialog
+        title="提示"
+        :visible.sync="dialogVisible"
+        width="30%"
+        :before-close="handleClose">
+        <el-form ref="newform"  :model="newform" :rules="rules" size="medium">
+          <el-form-item size="large" label="公司名称" prop="name">
+              <el-input v-model="newform.name"></el-input>
+          </el-form-item>
+          <el-form-item size="large" label="投标价" prop="num">
+              <el-input v-model.number="newform.num"></el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="add">确 定</el-button>
+        </span>
+      </el-dialog>
   </div>
 </template>
 <script>
@@ -89,44 +90,36 @@ export default {
   props: [],
   data() {
     return {
+      dialogVisible: false,
       formData: {
-        mobile: undefined,
-        field118: null,
-        field119: undefined,
+        max: null,
+        min: null,
         num: 0
+      },
+      newform: {
+        name: "",
+        num: null
+      },
+      form: {
+        name: "",
+        num: ""
       },
       query: {
         max:0,
         min: 0
       },
       rules: {
-        mobile: [{
+        name: [{
           required: true,
-          message: '请输入手机号',
-          trigger: 'blur'
-        }, {
-          pattern: /^1(3|4|5|7|8|9)\d{9}$/,
-          message: '手机号格式错误',
+          message: '请输入公司名称',
           trigger: 'blur'
         }],
-        field118: [{
+        num: [{
           required: true,
-          message: '日期范围不能为空',
-          trigger: 'change'
-        }],
-        field119: [{
-          required: true,
-          message: '请选择选择',
+          message: '请输入公司投标价',
           trigger: 'change'
         }],
       },
-      field119Options: [{
-        "label": "选项一",
-        "value": 1
-      }, {
-        "label": "选项二",
-        "value": 2
-      }],
       list: [
         {
           name: "四川君羊建设集团有限公司",
@@ -181,37 +174,98 @@ export default {
       ]
     }
   },
-  filters: {
-    
+  watch: {
+    formData: {
+      handler(){
+        this.computedNum();
+      },
+      deep: true
+    }
     
   },
   created() {},
   methods: {
-    submitForm() {
+    // 计算
+    computedNum() {
       const len = this.list.length;
       const total = this.list.reduce((i, t) => i + t.num,0);
       this.query.min = Math.floor(total/len*this.formData.min);
+      console.log(this.query.min)
       this.query.max =Math.floor(this.formData.num*this.formData.max);
-      this.$refs['elForm'].validate(valid => {
+    },
+    // 新增数据
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
         if (!valid) return
         // TODO 提交表单
+        this.list.push({
+          ...this.form
+        });
+        this.resetForm(formName)
       })
     },
-    resetForm() {
-      this.$refs['elForm'].resetFields()
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
     },
-    add(){},
     proportions(val) {
       const result = (100 - (val/this.formData.num*100)).toFixed(2);
       return `${result}%`
     },
+    // 确定
+    add(){
+      this.$confirm('此操作将永久修改该数据, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // TODO 提交修改数据接口
+        this.list[this.newform.index].name = this.newform.name;
+        this.list[this.newform.index].num = this.newform.num;
+        console.log(this.newform.index)
+        this.dialogVisible = false;
+      })
+      
+    },
+    // 删除数据按钮
+    remove(row) {
+      this.$confirm('此操作将永久删除该数据, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // TODO 删除数据接口
+        this.list = this.list.filter( i => i.name !== row.name)
+      })
+    },
+    // 修改数据按钮
+    edit(row){
+      this.dialogVisible = true;
+      this.newform.name = row.name;
+      this.newform.num = row.num;
+      this.newform.id = id;
+    },
+    // 关闭弹窗回调
+    handleClose(){
+      this.resetForm("newform")
+    },
+    // 筛选双底线
     filterHandler(value, row, column){
       const property = column['property'];
       return row[property] >= this.query.min || row[property]>= this.query.max;
+    },
+    // 前三名颜色
+    tableRowClassName({row, rowIndex}) {
+      if (rowIndex <= 2) {
+        return 'top';
+      }
+      return '';
     }
   }
 }
 
 </script>
-<style>
+<style lang="scss">
+.top{
+  background-color:oldlace !important;
+}
 </style>
